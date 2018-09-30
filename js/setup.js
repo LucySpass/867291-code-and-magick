cac914df5e02c2f8573b6e05860c4dde05b84bdd
ivana.begovic@yahoo.com <ivana.begovic@yahoo.com>
2018-09-30 21:48:53 +0300

    Test branch!

--- js/setup.js
+++ js/setup.js
@@ -8,118 +8,148 @@ var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 var NUMBER_OF_WIZARDS = 4;
 
-var isDialogOpen = true;
+var dialogModule = (function () {
+  var querySelector = '';
 
-function getValue(array) {
-  return deleteUsedArrayValue(array, getRandomArrayValue(array));
-}
+  return {
+    setQuerySelector: function (value) {
+      querySelector = document.querySelector(value);
+    },
 
-function getRandomArrayValue(array) {
-  var index = Math.floor(Math.random() * array.length);
-  return array[index];
-}
+    open: function () {
+      querySelector.classList.remove('hidden');
+    },
 
-function deleteUsedArrayValue(array, value) {
-  array.splice(array.indexOf(value), 1);
-  return value;
-}
-
-function closeDialog() {
-  document.querySelector('.setup').classList.add('hidden');
-  isDialogOpen = false;
-}
-
-function openDialog() {
-  document.querySelector('.setup').classList.remove('hidden');
-  isDialogOpen = true;
-}
-
-function changeColor(element, tempArray, constArray, input) {
-  element.addEventListener('click', function () {
-    if (tempArray.length === 0) {
-      tempArray = constArray.slice();
+    close: function () {
+      querySelector.classList.add('hidden');
     }
-    var randomValue = getValue(tempArray);
-
-    if (constArray.length === FIREBALL_COLORS.length && constArray.every(function (value, index) {
-      return value === FIREBALL_COLORS[index];
-    })) {
-      element.style.background = randomValue;
-    } else {
-      element.style.fill = randomValue;
-    }
-    input.value = randomValue;
-  });
-}
-
-function main() {
-  var setup = document.querySelector('.setup');
-  var similarListElement = setup.querySelector('.setup-similar-list');
-
-  var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
-  var inputWizardCoat = document.querySelector('.setup-player input[name=coat-color]');
-  var coatColors = WIZARD_COAT_COLOR.slice();
-
-  var wizardEye = document.querySelector('.setup-wizard .wizard-eyes');
-  var inputWizardEyes = document.querySelector('.setup-player input[name=eyes-color]');
-  var eyesColors = WIZARD_EYES_COLOR.slice();
-
-  var fireballElement = document.querySelector('.setup-fireball-wrap');
-  var inputFireball = document.querySelector('.setup-fireball-wrap input[name=fireball-color]');
-  var fireballColors = FIREBALL_COLORS.slice();
-
-  var similarWizardTemplate = document
-    .querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
-
-  var generateWizard = function () {
-    var wizard = similarWizardTemplate.cloneNode(true);
-    wizard.querySelector('.setup-similar-label').textContent = getValue(WIZARD_NAMES) + ' ' + getValue(WIZARD_SURNAMES);
-    wizard.querySelector('.wizard-coat').style.fill = getValue(coatColors);
-    wizard.querySelector('.wizard-eyes').style.fill = getValue(eyesColors);
-
-    return wizard;
   };
+})();
 
-  var fragment = document.createDocumentFragment();
+/**
+ * Из-за этого модуля происходит странное поведение.
+ * Работает только последний вызов setValues функции.
+ * @type {{setValues}}
+ */
+var colorChangeModule = (function () {
+  var _element;
+  var _tempArray;
+  var _constArray;
+  var _input;
 
-  for (var i = 0; i < NUMBER_OF_WIZARDS; i++) {
-    fragment.appendChild(generateWizard());
+  function changeColor() {
+    _element.addEventListener('click', function () {
+      if (_tempArray.length === 0) {
+        _tempArray = _constArray.slice();
+      }
+      var randomValue = initModule.getValue(_tempArray);
+      if (_constArray.length === FIREBALL_COLORS.length && _constArray.every(function (value, index) {
+        return value === FIREBALL_COLORS[index];
+      })) {
+        _element.style.background = randomValue;
+      } else {
+        _element.style.fill = randomValue;
+      }
+      _input.value = randomValue;
+    });
   }
 
-  similarListElement.appendChild(fragment);
-
-  setup.classList.remove('hidden');
-  setup.querySelector('.setup-similar').classList.remove('hidden');
-
-  document.querySelector('.setup-open').addEventListener('click', function () {
-    openDialog();
-  });
-
-  document.querySelector('.setup-close').addEventListener('click', function () {
-    closeDialog();
-  });
-
-  document.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-      if (document.activeElement.classList.contains('setup-close') ||
-        document.activeElement.classList.contains('setup-submit')) {
-        closeDialog();
-      }
-      if (document.activeElement.classList.contains('setup-open-icon')) {
-        openDialog();
-      }
+  return {
+    setValues: function (constArray, selector, inputSelector) {
+      _element = document.querySelector(selector);
+      _input = document.querySelector(inputSelector);
+      _tempArray = constArray.slice();
+      _constArray = constArray;
+      changeColor();
     }
-    if (event.key === 'Escape' && isDialogOpen &&
-      !document.activeElement.classList.contains('setup-user-name')) {
-      closeDialog();
+  };
+})();
+
+var initModule = (function () {
+  function getValue(array) {
+    return deleteUsedArrayValue(array, getRandomArrayValue(array));
+  }
+
+  function getRandomArrayValue(array) {
+    var index = Math.floor(Math.random() * array.length);
+    return array[index];
+  }
+
+  function deleteUsedArrayValue(array, value) {
+    array.splice(array.indexOf(value), 1);
+    return value;
+  }
+
+  return {
+    getValue: function (array) {
+      return getValue(array);
+    },
+    main: function () {
+      var setup = document.querySelector('.setup');
+      var similarListElement = setup.querySelector('.setup-similar-list');
+      var coatColors = WIZARD_COAT_COLOR.slice();
+      var eyesColors = WIZARD_EYES_COLOR.slice();
+
+      /**
+       * Из этих трех вызовов цвет меняет только элемент последнего.
+       * Так должно происходить и можно ли сделать чтобы логика была
+       * правильная этим способом?
+       */
+      colorChangeModule.setValues(WIZARD_COAT_COLOR, '.setup-wizard .wizard-coat', '.setup-player input[name=coat-color]');
+      colorChangeModule.setValues(WIZARD_EYES_COLOR, '.setup-wizard .wizard-eyes', '.setup-player input[name=eyes-color]');
+      colorChangeModule.setValues(FIREBALL_COLORS, '.setup-fireball-wrap', '.setup-fireball-wrap input[name=fireball-color]');
+
+      var similarWizardTemplate = document
+        .querySelector('#similar-wizard-template')
+        .content
+        .querySelector('.setup-similar-item');
+
+      var generateWizard = function () {
+        var wizard = similarWizardTemplate.cloneNode(true);
+        wizard.querySelector('.setup-similar-label').textContent = getValue(WIZARD_NAMES) + ' ' + getValue(WIZARD_SURNAMES);
+        wizard.querySelector('.wizard-coat').style.fill = getValue(coatColors);
+        wizard.querySelector('.wizard-eyes').style.fill = getValue(eyesColors);
+
+        return wizard;
+      };
+
+      var fragment = document.createDocumentFragment();
+
+      for (var i = 0; i < NUMBER_OF_WIZARDS; i++) {
+        fragment.appendChild(generateWizard());
+      }
+
+      dialogModule.setQuerySelector('.setup');
+
+      similarListElement.appendChild(fragment);
+
+      setup.classList.remove('hidden');
+      setup.querySelector('.setup-similar').classList.remove('hidden');
+
+      document.querySelector('.setup-open').addEventListener('click', dialogModule.open());
+      document.querySelector('.setup-close').addEventListener('click', function () {
+        dialogModule.close();
+      });
+      document.querySelector('.setup-open').addEventListener('click', function () {
+        dialogModule.open();
+      });
+
+      document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+          if (document.activeElement.classList.contains('setup-close') ||
+            document.activeElement.classList.contains('setup-submit')) {
+            dialogModule.close();
+          }
+          if (document.activeElement.classList.contains('setup-open-icon')) {
+            dialogModule.open();
+          }
+        }
+        if (event.key === 'Escape' && !document.activeElement.classList.contains('setup-user-name')) {
+          dialogModule.close();
+        }
+      });
     }
-  });
+  };
+})();
 
-  changeColor(wizardCoat, coatColors, WIZARD_COAT_COLOR, inputWizardCoat);
-  changeColor(wizardEye, eyesColors, WIZARD_EYES_COLOR, inputWizardEyes);
-  changeColor(fireballElement, fireballColors, FIREBALL_COLORS, inputFireball);
-}
-
-main();
+initModule.main();
